edffc72018d1f0a6602aae107efafe85fc94821d
benzo18 <161372612+benzo18@users.noreply.github.com>
2024-11-14 16:31:17 +0200

    refactor: Remove backend code
    This commit removes the backend code from this repository, as it will be hosted separately on Render. This separation allows for independent deployment and management of the frontend and backend components of the application.

--- backend/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    mongoose.set('debug', true);
-    await mongoose.connect('mongodb+srv://ubuntu_db_admin:GJa0m8s1fldcnkLJ@bigbite-app-cluster.i01za.mongodb.net/BIGBITE_WEB').then (()=>console.log("DB connected"));
-}
