10497157834f53644790370fb79a82b5b2bdea24
benzo18 <161372612+benzo18@users.noreply.github.com>
2024-11-14 20:43:41 +0200

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -6,7 +6,7 @@ import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 import bigbite from './bigbite.png'
 
-export const url = 'https://bigbite-site.onrender.com'
+export const url = 'https://bigbite-site-backend.onrender.com'
 export const currency = 'R'
 
 export const assets ={
