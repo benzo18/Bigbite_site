429c2c219ff8c075ad3c3ac138dcf630bd5a46a0
benzo18 <161372612+benzo18@users.noreply.github.com>
2024-11-15 09:40:03 +0200

    refactor: update icon img
    app proper icon image

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,7 +5,7 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 import bigbite from './bigbite.png'
-import admin_icon from './add_icon.png'
+import admin_icon from './admin_icon.png'
 
 export const url = 'https://bigbite-site-backend.onrender.com'
 export const currency = 'R'
