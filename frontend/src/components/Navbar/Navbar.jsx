c29b9f0b6b12d356b7f452d6ab6ffa5e7f845d76
benzo18 <161372612+benzo18@users.noreply.github.com>
2024-11-15 09:30:30 +0200

    refactor: navbar code
    update navbar code

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -38,7 +38,7 @@ const logout = () =>{
         :<div className='navbar-profile'>
           <img src={assets.profile_icon} alt="" />
             <ul className="nav-profile-dropdown">
-              <li><img src={assets.logout_icon} alt="" /><p>Orders</p></li>
+              
               <hr />
               <li onClick={logout} ><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
             </ul>
