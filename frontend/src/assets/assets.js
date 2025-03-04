038b56e0f79cddd83ccae94172ba049175ee621e
benzo18 <161372612+benzo18@users.noreply.github.com>
2025-03-05 01:16:18 +0200

    update footer.jsx
    minor changes to the footer

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -62,6 +62,7 @@ import bag_icon from './bag_icon.png'
 import logout_icon from './logout_icon.png'
 import parcel_icon from './parcel_icon.png'
 import instagram_logo from './instagram_logo.jpg'
+import userManual from './userManual.pdf'
 
 export const assets = {
     bigbitebackground,
@@ -85,7 +86,8 @@ export const assets = {
     logout_icon,
     bag_icon,
     parcel_icon,
-    instagram_logo
+    instagram_logo,
+    userManual
     
 }
 
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,20 +7,29 @@ const Footer = () => {
     <div className='footer' id='footer'>
       <div className="footer-content">
         <div className="footer-content-left">
-            <img src={assets.bigbite} alt="" className='logo' />
-           <div className="footer-social-icons">
-            <img src={assets.facebook_icon} alt="" />
-            <img src={assets.instagram_logo} alt="" />
-            <img src={assets.twitter_icon} alt="" />
-           </div>
+          <img src={assets.bigbite} alt="" className='logo' />
+          <div className="footer-social-icons">
+
+            <a href="https://www.instagram.com/bigbitecafeza/" target="_blank" rel="noopener noreferrer">
+              <img src={assets.instagram_logo} alt="" />
+            </a>
+
+          </div>
         </div>
-       
+
         <div className="footer-content-right">
-            <h2>GET IN TOUCH</h2>
-            <ul>
-                <li>+27 11-598-7070</li>
-                <li>bigbite@gmail.com</li>
-            </ul>
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+27 11-598-7070</li>
+            <li>bigbite@gmail.com</li>
+          </ul>
+
+          <div className="user-manual">
+            <a href={assets.userManual} download="BigBite_User_Manual.pdf">
+              Download User Manual
+            </a>
+          </div>
+
         </div>
       </div>
       <hr />
