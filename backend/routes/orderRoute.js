73e8f184879c082bd29f9e533545075d2ac2c3a3
benzo18 <161372612+benzo18@users.noreply.github.com>
2025-03-11 17:45:50 +0200

    update orderRoute.js
    orderRoute.js should be able to import authMiddleware correctly now

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import authMiddleware from "../middleware/auth.js";
+import { authMiddleware } from "../middleware/auth.js"; // Import as named export
 import { placeOrder,verifyOrder,userOrder,listOrders,updateStatus } from "../controllers/orderController.js";
 import orderModel from "../models/orderModel.js";
 
